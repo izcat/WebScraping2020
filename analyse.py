01b2997df554d4d9f51303e4d86deee72f4c06cb
izcat <de.ep@qq.com>
2020-03-11 21:33:43 +0800

    analyse.py 增加日期识别

--- analyse.py
+++ analyse.py
@@ -31,10 +31,17 @@ def showPLT(x, y, sig='.', xlabel='', ylabel='', title=''):
 全市核酸检测 = []
 发热门诊接诊 = []
 现有确诊病例 = []
-
 确诊率 = []
-ConfirmedofDistrict = [list() for i in range(13)]
+
+eachDistrict = None
+# eachDistrict 是一个长度为天数，每个元素为13元组的列表
+# 每一个元组为每一天武汉市13个区的确诊数据
+
+ConfirmedofDistrict =  [list() for i in range(13)]
 newConfirmedofDistrict = [list() for i in range(13)]
+# ConfirmedofDistrict 是二维数组 共13行
+# 每行为一个区的全部每日确诊数据
+
 
 districtRe = '江岸区(\d+)例、江汉区(\d+)例、硚口区(\d+)例、汉阳区(\d+)例、武昌区(\d+)例、青山区(\d+)例、洪山区(\d+)例、东西湖区(\d+)例、蔡甸区(\d+)例、江夏区(\d+)例、黄陂区(\d+)例、新洲区(\d+)例、武汉开发区\(汉南\)(\d+)例'
 districtName = ['江岸区', '江汉区', '硚口区', '汉阳区', '武昌区', '青山区', '洪山区', '东西湖区', '蔡甸区', '江夏区', '黄陂区', '新洲区', '武汉开发区(汉南)']
@@ -52,74 +59,57 @@ with open('data.txt', 'r', encoding='UTF-8') as f:
 	totalConfirmed = re.findall('现有确诊病例(\d+)', txt)
 	days = re.findall('2020年(\d+)月(\d+)日0-24', txt)
 	days = list(map(lambda x:x[0]+'.'+x[1], days))
-	
 	# print(days)
+	
 	eachDistrict = re.findall(districtRe, txt)
+	# eachDistrict 是一个元素为13元组的列表
+	# 每一个元组为每一天武汉市13个区的确诊数据
+	# 确诊数据类型为'str'
+	eachDistrict = list(map(lambda x:tuple(map(int, x)), eachDistrict))
+	# print(eachDistrict)
+	# 以下三行为上面的等价操作（忽略tuple类型）
+	# for i in range(len(eachDistrict)):
+	# 	for j in range(len(eachDistrict[0])):
+	# 		ConfirmedofDistrict[i] += [int(eachDistrict[i][j])]
 
 	全市核酸检测 = list(map(int, inspectPCR))
 	发热门诊接诊 = list(map(int, feverEveryday))
 	现有确诊病例 = list(map(int, totalConfirmed))
-
-	# print(inspectPCR)
-	print(eachDistrict)
-	
-	for i in range(len(eachDistrict)):
-		eachDistrict[i] = list(map(int, eachDistrict[i]))
-		item = eachDistrict[i]
-		if item[8]<10:
-			for j in range(len(item)):
-				newConfirmedofDistrict[j].append(item[j])
-			continue
-		for j in range(len(item)):
-			ConfirmedofDistrict[j].append(item[j])
-
-			if len(ConfirmedofDistrict[j])>1:
-				newConfirmedofDistrict[j].append(ConfirmedofDistrict[j][-1]-ConfirmedofDistrict[j][-2])
-	# print(len(eachDistrict))
-
-
-
 	新增确诊病例 = list(map(int, newConfirmed))
 	新增出院病例 = list(map(int, newCured))
 	新增死亡病例 = list(map(int, newDead))
 	新增疑似病例 = list(map(int, newSuspected))
-	# print(新增确诊病例)
-	# print(新增出院病例)
-	# print(新增死亡病例)
-	# print(新增疑似病例)
-	# print(txt)
 
-	# txt.find('新增确诊病例')
 	
-	# data = txt.split('\n\n')
-
-	# print(len(data))
-
-	# for i in data:
-	# 	print(i, end='\n----\n')
-
-
-
-
+	# eachDistrict行列转换 ==> ConfirmedofDistrict
+	# ConfirmedofDistrict 为二维数组 共13行
+	# 每行为一个区的全部每日确诊数据
+	for i in range(len(eachDistrict)):
+		eachDistrict[i] = list(map(int, eachDistrict[i]))
+		item = eachDistrict[i]
+		for j in range(len(item)):
+			ConfirmedofDistrict[j].append(item[j])
+			# newConfirmedofDistrict[j].append(ConfirmedofDistrict[j][-1]-ConfirmedofDistrict[j][-2] if len(ConfirmedofDistrict[j])>1
+			# 								 else ConfirmedofDistrict[j][0])
+			# 新增从23日开始
+			if len(ConfirmedofDistrict[j])>1:
+				newConfirmedofDistrict[j].append(ConfirmedofDistrict[j][-1]-ConfirmedofDistrict[j][-2])
+			
+	print(ConfirmedofDistrict)
+	# print(len(eachDistrict))
 
 
 # 现有确诊变化
 def showNowLeft():
-	print(现有确诊病例)
-	for i in 现有确诊病例:
-		print(i)
-	aaaa = [新增出院病例[i]+新增死亡病例[i] for i in range(len(新增出院病例))]
-	print(aaaa)
-	print(新增出院病例)
-	days = ['2.2'+str(i) for i in range(1,10)]
-	for i in range(1, 6):
-		days.append('3.'+str(i))
-	# days = list(map(float, days))
-	plt.plot(days, 现有确诊病例, label='totalConfirmedCases')
-	days += ['3.'+str(i) for i in range(3, 20)]
-	plt.xticks(days)
+	# aaaa = [新增出院病例[i]+新增死亡病例[i] for i in range(len(新增出院病例))]
+	# print(aaaa)
+	
+	plt.plot(days, 现有确诊病例, label='Confirmed Cases left now')
+	
 	yyy = [i for i in range(0, 40001, 4000)]
+	plt.xticks(days)
 	plt.yticks(yyy)
+
 	plt.legend()
 	plt.show()
 
@@ -145,10 +135,10 @@ def showConfirmRatio():
 def showNewDistrict():
 	import matplotlib.pyplot as plt
 	# 2.23日开始
-	days = ['2.2'+str(i) for i in range(3,10)]
-	for i in range(1, len(newConfirmedofDistrict)-2):
-		days.append('3.'+str(i))
-	# plt.plot(ConfirmedofDistrict)
+	# days = ['2.2'+str(i) for i in range(3,10)]
+	# for i in range(1, len(newConfirmedofDistrict)-2):
+	# 	days.append('3.'+str(i))
+	days.pop(0)
 	for i in range(len(newConfirmedofDistrict)):
 		# if i==2 or i==3 or i==4 or i==6:
 			# continue
@@ -159,24 +149,35 @@ def showNewDistrict():
 	plt.legend()
 	plt.show()
 
-# 每个区新增情况
+
+# 每个区累计确诊情况
 def showDistrict():
 	import matplotlib.pyplot as plt
 	# 2.22开始
-	days = ['2.2'+str(i) for i in range(2,10)]
-	for i in range(1, len(ConfirmedofDistrict)-2):
-		days.append('3.'+str(i))
-	# plt.plot(ConfirmedofDistrict)
+	# days = ['2.2'+str(i) for i in range(2,10)]
+	# for i in range(1, len(ConfirmedofDistrict)-2):
+	# 	days.append('3.'+str(i))
+
+	# 直接使用 glabal days 和 eachDistrict
+	# 自动绘制多维数据，但无法添加标签
+	# plt.plot(days, eachDistrict)
+	
+	# plt.xlabel('日期')
+	# plt.ylabel('每日累计确诊')
+	# plt.legend()
+	# plt.show()
+
 	for i in range(len(ConfirmedofDistrict)):
 		# if i==2 or i==3 or i==4 or i==6:
-			# continue
+		# 	continue
 		plt.plot(days, ConfirmedofDistrict[i], label=districtName[i])
-
 	plt.xlabel('日期')
-	plt.ylabel('每日新增确诊')
+	plt.ylabel('每日累计确诊')
 	plt.legend()
 	plt.show()
 
 
 # showDistrict()
-showConfirmRatio()
+# showNewDistrict()
+# showConfirmRatio()
+# showNowLeft()
